a4d7be47104601faff137dbc78e6bd039cd9b372
jiaoxu <2356692289@qq.com>
2023-08-14 15:22:25 +0800

    8-14modi

--- User-portrait/generate_persona.py
+++ User-portrait/generate_persona.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from wordcloud import WordCloud, ImageColorGenerator
 
-text = "客户类型, 性别, 是否农户, 工作,职务, 有无子女,是否有通讯地址, 是否有家庭电话, 是否有手机号码,是否有配偶手机号\
-     是否有公司电话,住房情况, 计息周期, 还款方式, 担保方式, 贷款形式,产品名称"
+text = "男性, 居住上海, 开户年限3年,建筑行业, 持有活期存款,持有定期存款, 持有结构性存款, 大量购买表外理财,少量购买保险\
+     当年开通手机银行,手机银行活跃用户, 无贷款, 代发薪存款留存率高,复购概率高"
 
 
 # 加载背景图
-color_mask = np.array(Image.open("./person.png"))
+color_mask = np.array(Image.open("./personPay.png"))
 wc = WordCloud(
     mask=color_mask,
     background_color='white',
@@ -22,5 +22,5 @@ image_colors = ImageColorGenerator(color_mask)
 # 在只设置mask的情况下 会得到一个拥有图片形状的词云 axis默认为on 会开启边框
 plt.imshow(wc, interpolation="bilinear")
 plt.axis("off")
-plt.savefig("./result.jpg")
+plt.savefig("./resultPay.jpg")
 
